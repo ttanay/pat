4a611cb04ca64b500e457e826b3420f5cf43c592
Tanay Tummalapalli <ttanay100@gmail.com>
2024-02-18 01:17:59 +0530

    Add pretty printing and scroll mode
    The response can be pretty printed.
    A custom formatter can also be specified.
    
    The reponse can also be displayed in a terminal pager.
    A customer terminal pager can also be specified.
    
    If curl fails for some reason, the error is also displayed
    as part of output.

--- pat_explore.sh
+++ pat_explore.sh
@@ -1,13 +1,5 @@
 #!/usr/bin/bash
 
-
-# Flow:
-# A request is specified in a file with extension `curlf`
-# The tool runs the file and:
-#   1.  Dumps response to stdout
-#   2.  Pretty prints response to stdout
-#   3.  Opens it in a scrollable pane
-
 usage=$(cat << EOF
 Pat: An HTTP API development toolkit
 
@@ -18,6 +10,9 @@ and runs them.
 Supported options:
     * -f: Filepath to run requests from (default present dir)
     * -s: Open the response in a terminal pager
+        The response is piped to a terminal pager.
+        You can specify your own with the TERM_PAGE variable.
+        The default is \`less\`.
     * -p: Pretty print the response.
         The response is piped to a pretty printer.
         You can specify your own with the RESP_FMT variable.
@@ -25,6 +20,10 @@ Supported options:
     * -h: Print help message
 EOF
 )
+
+# Backends
+formatter=${RESP_FMT:-"jq ."}
+scroller=${TERM_PAGE:-"less"}
 
 # COLORS
 RED='\033[0;31m'
@@ -36,40 +35,66 @@ filepath=`pwd`
 curl_args=""
 # TODO: Add option to show curl progress
 curl_progress=false
+scroll_mode=false
 pretty_print=false
-while getopts f:hp opt; do
+while getopts f:hps opt; do
     case $opt in
         h) echo "$usage"; exit;;
         f) $filepath=${OPTARG};;
         p) pretty_print=true;;
+        s) scroll_mode=true;;
         :) echo "Missing argument for option -$OPTARG"; echo "$usage"; exit 1;;
        \?) echo "Unknown option -$OPTARG"; echo "$usage"; exit 1;;
     esac
 done
 
-formatter=${RESP_FMT:-"jq ."}
+# Unset flags if commands not available
+if [ "$pretty_print" = true ] && ! command -v "${formatter%% *}" &> /dev/null ; then
+    echo "Specified pretty printer \`$formatter\` not found; Skipping pretty printing..."
+    pretty_print=false
+fi
+if [ "$scroll_mode" = true ] && ! command -v "${scroller%% *}" &> /dev/null ; then
+    echo "Specified terminal pager \`$scroller\` not found; Skipping terminal pager..."
+    scroll_mode=false
+fi
 
 if ! "$curl_progress"; then
-    curl_args+="-s"
+    curl_args+="--silent --show-error --fail"
 fi
 
 shift $(( OPTIND - 1 ))
 
+exit_code=0
 requests=(`find "$filepath" -type f -name "*.curlf" -and -not -name "$0"`)
 for request in "${requests[@]}"
 do
+    echo "Running $request"
     # Read file
     request_spec=`cat $request`
 
     # Concatenate with curl
     curl_cmd="curl ${curl_args} ${request_spec}"
     # Run curl request
-    response=$(eval "$curl_cmd")
+    response=$(eval "$curl_cmd" 2>&1)
+
+    curl_exit_code=`echo $?`
+    if [ "$curl_exit_code" != 0 ]; then
+        echo "Curl encountered an error: "
+        echo "$response"
+        exit_code=1
+        continue
+    fi
 
     # Print response in desired manner
-    if "$pretty_print"; then
+    if [ "$pretty_print" = true ] && [ "$scroll_mode" = true ]; then
+        echo "$response" | (eval "$formatter") | (eval "$scroller")
+    elif [ "$pretty_print" = true ] && [ "$scroll_mode" = false ]; then
         echo "$response" | (eval "$formatter")
+    elif [ "$pretty_print" = false ] && [ "$scroll_mode" = true ]; then
+        echo "$response" | (eval "$scroller")
     else
         echo "$response"
     fi
 done
+
+exit $exit_code

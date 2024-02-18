3aa2fb506c49fc6d9b96858417f066c5c65377e7
Tanay Tummalapalli <ttanay100@gmail.com>
2024-02-18 18:49:05 +0530

    Add some more nuance to test

--- test/server.py
+++ test/server.py
@@ -1,13 +1,31 @@
-from flask import Flask, jsonify
+from flask import Flask, jsonify, abort
 
 app = Flask(__name__)
 
-@app.get("/person")
-def get_person():
-    person = {
+persons = [
+    {
         "id": 1,
         "name": "John Carmack",
         "dob": "1970-08-21",
         "occupation": "doom-god"
+    },
+    {
+        "id": 2,
+        "name": "Edgar Djikstra",
+        "dob": "1930-05-11",
+        "occupation": "scientist"
+
     }
+]
+
+
+@app.errorhandler(404)
+def resource_not_found(e):
+    return jsonify(error=str(e)), 404
+
+@app.get("/person/<int:person_id>")
+def get_person(person_id):
+    if person_id > len(persons):
+        return abort(404, f"Person with id {person_id} not found")
+    person = persons[person_id - 1]
     return jsonify(person)
